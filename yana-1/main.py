5c33af53cfe9690f532bc599544a4844d9f6bd66
c1cadoidea <97469012+c1cadoidea@users.noreply.github.com>
2024-02-20 22:19:41 +0200

    all is ok

--- yana-1/main.py
+++ yana-1/main.py
@@ -1,14 +1,3 @@
-import numpy as np
-
-def unravel_index_custom(index, shape):
-    coords = []
-    for dim_size in reversed(shape):
-        coords.append(index % dim_size)
-        index //= dim_size
-    return tuple(reversed(coords))
-
-element_number = 9
-shape = (2, 3, 2)
-
-index = unravel_index_custom(element_number, shape)
-print(index)
+print("Hello World!")
+import this 
+ # noqa: E402
