01a081eedf544fc109db19c7a50dd2c5d585a3d5
c1cadoidea <sky.yarotskyi@gmail.com>
2024-03-19 22:51:46 +0200

    //

---
+++ lab1/task5.py
@@ -0,0 +1,7 @@
+amount_50 = int(input("How many coins of 50 cents do you have? : "))
+amount_25 = int(input("How many coins of 25 cents do you have? : "))
+amount_10 = int(input("How many coins of 10 cents do you have? : "))
+amount_5 = int(input("How many coins of 5 cents do you have? : "))
+
+all_money = amount_50 * 0.5 + amount_25 * 0.25 + amount_10 * 0.1 + amount_5 * 0.05
+print("You have :", all_money, "grivnas")
